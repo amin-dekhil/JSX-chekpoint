fce4db5cd01d95e21d466cf7f78eb90a79049cac
amin-dekhil <dekmohamedamin@gmail.com>
2021-04-18 15:23:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import image from "./download.jfif"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{border:'solid 1px black' , maxWidth:'100vw'}}>
+
+<h1 className="title red">Your name here</h1>
+
+<br />
+
+<img src="\applis-retouche-android-ios-big.jpg" />
+
+<br />
+
+<img src={image} />
+
+</div>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/TDiXxsQ0w2Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+
     </div>
   );
 }
